b839ff7d2acd4a06e4cabc40fd8bac3b5d78c2ca
Ahn HyungJun <103185987+AHNDOIL@users.noreply.github.com>
2023-05-30 15:11:29 +0900

    calculate fire danger index algorithm

---
+++ iot/cal/dhtAlgorithm/dangerIndex.py
@@ -0,0 +1,19 @@
+
+
+def calculate_fire_danger_index(temperature, relative_humidity):
+    if temperature >= 35 and relative_humidity < 25:
+        return "Red"  # 온도 35도 이상, 상대습도 25% 미만: 매우 높음 등급
+    elif temperature >= 28 and relative_humidity < 40:
+        return "Yellow"  # 온도 28도 이상, 상대습도 40% 미만: 높음 등급
+    else:
+        return "Green"  # 나머지 경우: 보통 등급
+
+# 예시 입력값
+current_temperature = float(input())
+current_relative_humidity = float(input())
+
+# 산불위험지수 계산
+fire_hazard_index = calculate_fire_danger_index(current_temperature, current_relative_humidity)
+
+# 결과 출력
+print("산불위험지수:", fire_hazard_index)
