218f0ec3764e22b50191301cc1f43e21c3d5112b
jayang <qud1251@gmail.com>
2023-05-31 13:52:27 +0900

    secret

--- iot/flask_application/app.py
+++ iot/flask_application/app.py
@@ -3,10 +3,13 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+with open('secret.txt', 'r') as file:
+	SECRET = file.readline().strip()
+
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'qud1251@likelion.org'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = SECRET
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
