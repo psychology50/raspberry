0a58098fe2b354755e63337aae87268749096cae
jayang <qud1251@gmail.com>
2023-05-31 14:06:04 +0900

    debug false

--- iot/flask_application/app.py
+++ iot/flask_application/app.py
@@ -50,6 +50,6 @@ def send_mail():
 	return 'send mail successfully!'
 
 if __name__ == '__main__':
-	app.debuf = True # for develop
+	app.debug = False # for develop
 	app.run(host="127.0.0.1", port="8080")
 
